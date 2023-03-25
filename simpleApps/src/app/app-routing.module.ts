16c311b3d8667be6db1a6a15ad9005f7559f4ae2
miku <miku@mail.com>
2023-03-24 23:00:15 -0700

    praktikum-4

--- simpleApps/src/app/app-routing.module.ts
+++ simpleApps/src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
   {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'register',
     pathMatch: 'full'
   },
 ];
