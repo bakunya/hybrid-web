ef7a991d3cab53325f9226e1b9027f109b22adc1
miku <miku@mail.com>
2023-03-31 23:13:17 -0700

    pertemuan-4

--- liveScore/src/app/tab1/tab1.page.ts
+++ liveScore/src/app/tab1/tab1.page.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { MATCH, MATCH_TYPE } from '../../assets/data'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent],
+  imports: [IonicModule, CommonModule, ExploreContainerComponent],
 })
 export class Tab1Page {
+  public match: MATCH_TYPE = MATCH
+
   constructor() {}
+
 }
